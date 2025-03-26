d24bb89fdc16f56d37d5cab9c3255d20cb8da6b4
whong4 <whong4@gmu.edu>
2025-03-25 22:38:35 -0400

    Initial SpringBoot Application

---
+++ src/main/java/com/example/hw/studentsurveyform/StudentSurveyFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw.studentsurveyform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSurveyFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSurveyFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw/studentsurveyform/StudentSurveyFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw.studentsurveyform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSurveyFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
